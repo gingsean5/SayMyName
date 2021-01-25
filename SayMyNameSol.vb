352304c5a6bcbf1a7dc2fdc02ee661c4965d11c9
FroYoMan <34078793+FroYoMan@users.noreply.github.com>
2021-01-25 08:56:40 -0700

    Add project files.

---
+++ SayMyNameSol.vb
@@ -0,0 +1,21 @@
+﻿
+Option Strict On
+Option Explicit On
+'Sean Gingerich
+'RCET0265
+'Fall 2020
+'Simple Calculator
+'
+Module SayMyNameSol
+
+    Sub Main()
+        Dim userName As String
+
+        'While problem = True
+        Console.WriteLine("What is your name?")
+        userName = Console.ReadLine()
+        Console.WriteLine($"You have a beautiful name, {userName}. ")
+        Console.Read()
+    End Sub
+
+End Module
